ad7f78d651d780782b5ed853d5d4f9a7166020be
djjbanx-laptop <jrh395@drexel.edu>
2018-04-14 19:51:11 -0400

    Rework Geocoder and write it myself

--- bot.js
+++ bot.js
@@ -1,6 +1,25 @@
 var keys = require('./keys');
 var fs = require("fs");
-var geocoder = require('geocoder');
+var request = require('request');
+
+geolocate = function (address, cbk) {
+    var uri = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&key="+keys.google.key;
+    request({
+        uri: uri
+    }, function(err,resp,body) {
+        if (err) return cbk(err);
+        var result;
+        try {
+            result = JSON.parse(body);
+        } catch (err) {
+            cbk(err);
+            return;
+        }
+        if (typeof cbk === "function") {
+            cbk(null,result);
+        }
+    });
+};
 
 var taxifare = new Map();
 
@@ -43,7 +62,7 @@ let endLat = 39.3514433;
 let endLng = -74.5751162;
 
 if (street1!=null && city1!=null && zip1!=null && state1!=null) {
-    geocoder.geocode(street1 + ', '+city1+', ' + state1+', ' + zip1, function (err, result) {
+    result = geolocate(street1+','+city1+','+state1+','+zip1, function(err, result) {
         console.log(result.results[0].geometry.location);
         startLat = result.results[0].geometry.location.lat;
         startLng = result.results[0].geometry.location.lng;
@@ -51,21 +70,20 @@ if (street1!=null && city1!=null && zip1!=null && state1!=null) {
     });
 }
 if (street2!=null && city2!=null && zip2!=null && state2!=null) {
-    geocoder.geocode(street2 + ', '+city2+', ' + state2+', ' + zip2, function (err, result) {
+    result = geolocate(street2 + ', '+city2+', ' + state2+', ' + zip2, function(err, result) {
         console.log(result.results[0].geometry.location);
         endLat = result.results[0].geometry.location.lat;
         endLng = result.results[0].geometry.location.lng;
         console.log(endLat + ' ' + endLng);
-
     });
 }
-
-console.log(taxifare.get('newyork'));
 
 let lowestCost = 100000000000;
 let dName = 'Ayyyyy';
 let rType = 'Ayyyyyyyy';
 let spicyBoy = 'Me';
+
+
 
 uber.estimates.getPriceForRouteAsync(startLat,startLng,endLat,endLng,1).then((data) => {
     var costs = data.prices;
