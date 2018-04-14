ca967803f64d6bbdbe0e1085491bc45ec8fc80b4
Daniel Schwartz <des338@drexel.edu>
2018-04-14 15:31:05 -0400

    added follow back, send dm

--- twitterWrapper.js
+++ twitterWrapper.js
@@ -8,19 +8,59 @@ class twitterWrapper {
 		this.stream = this.twitterAPI.stream('statuses/filter', { track: 'traveltimetogo'})
 	}
 
+	autoFollowBack(sn){
+		this.twitterAPI.post("friendships/create", {
+		screen_name: sn,
+	});
+		console.log("Followed " + sn);
+	}
+
+	sendDM(sn, txt) {
+		this.twitterAPI.post("direct_messages/new", {
+		screen_name: sn,
+		text: txt
+		
+	});
+		console.log("it worked");
+	}
+
 	startStream(){
+		var temp = this;
 		this.stream.on('tweet', function (tweet) {
 			var name = tweet.user.name;
 			var screen_name = tweet.user.screen_name;
+		 	temp.autoFollowBack(screen_name);
 		 	var place = tweet.place.full_name;
-			var geo = tweet.geo.coordinates;
+			var geo = null;
+			try{
+				geo = tweet.geo.coordinates;
+				
+			}catch(e){
+				temp.sendDM(screen_name, "Need to send tweet with precise location.");
+			}
 			console.log("Name: " + name + "\nScreen_Name: " + screen_name + "\nPlace: " + place);
 			console.log(geo);
+			
 		});
 	}
 
-	autoFollowBack(){
+	autoFollowBack(sn){
+		this.twitterAPI.post("friendships/create", {screen_name: sn}, function(err, response) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log("Followed " + sn);
+			}
+		});
+	}
+
+	sendDM(sn, txt) {
+		this.twitterAPI.post("direct_messages/new", {
+		screen_name: sn,
+		text: txt
 		
+	});
+		console.log("it worked");
 	}
 }
 
