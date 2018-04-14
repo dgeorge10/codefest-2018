b66cbf09ad00562fcc5034f03d76db5dc6161d12
Daniel Schwartz <des338@drexel.edu>
2018-04-14 13:44:09 -0400

    twitterStream

--- twitterWrapper.js
+++ twitterWrapper.js
@@ -5,18 +5,30 @@ var twitterAPI = bot.getTwitterAPI();
 class twitterWrapper {
 	constructor() {
 		this.twitterAPI = bot.getTwitterAPI();
-		console.log(this.twitterAPI);
-	}
+		var stream = this.twitterAPI.stream('statuses/filter', { track: 'traveltimetogo'})
 
-	getMentionName() {
-		var mentions = this.twitterAPI.get('statuses/mentions_timeline', { since_id: this.lastID }, function (err, data, response){
-			// this.lastID = data.id_str;
-			// console.log(data);	
-	
+		stream.on('tweet', function (tweet) {
+			console.log(tweet);
 		});
 	}
+
+	// constructor() {
+	// 	this.twitterAPI = bot.getTwitterAPI();
+	// 	this.lastID = this.twitterAPI.get('statuses/mentions_timeline', { since_id: this.lastID }, function (err, data, response){
+	// 		return data[0].user.id_str;
+	// 	});
+	// }
+
+	// getMentionName() {
+	// 	var mentions = this.twitterAPI.get('statuses/mentions_timeline', { since_id: this.lastID }, function (err, data, response){
+	// 		try{
+	// 			this.lastID = data[0].user.id_str;
+	// 			console.log(data);
+	// 		}
+					
+	// 	});
+	// }
 }
 
 var boo = new twitterWrapper();
 
-boo.getMentionName();
