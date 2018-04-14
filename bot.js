f4a810968504734b89621797477be824fb0edfe3
djjbanx-laptop <jrh395@drexel.edu>
2018-04-14 11:01:10 -0400

    Test node-uber, twit, and googlemaps

--- bot.js
+++ bot.js
@@ -1,2 +1,33 @@
+var keys = require('./keys')
 
-var Twit = require('twit');
+//var Lyft = require('node-lyft')
+//const lyft = new lyft(keys.lyft.client_token, keys.lyft.client_secret);
+
+var GoogleMapsAPI = require('googlemaps')
+var gmAPI = new GoogleMapsAPI({
+    key: keys.google.key,
+    stagger_time: 1000, // for elevationPath
+    encode_polylines:   false,
+    secure:             true // use https
+});
+
+
+var Uber = require('node-uber')
+var uber = new Uber({
+    client_id: keys.uber.client_id,
+    client_secret: keys.uber.client_secret,
+    server_token: keys.uber.server_token,
+    redirect_uri: '',
+    name: 'twitterbot',
+    language: 'en_US', // optional, defaults to en_US
+    sandbox: true, // optional, defaults to false
+});
+
+var Twit = require('twit')
+const twitter = new Twit({
+    consumer_key: keys.twitter.consumer_key,
+    consumer_secret: keys.twitter.consumer_secret,
+    access_token: keys.twitter.access_token,
+    access_token_secret: keys.twitter.access_token_secret,
+    timeout_ms: 60*1000,  // optional HTTP request timeout to apply to all requests.
+})
---
+++ keys.js
@@ -0,0 +1,22 @@
+module.exports = {
+    lyft: {
+        client_id: '-fGhLUA51FgR',
+        client_secret: 'H9F56Y1rO-8YVBY7ujYx7P0H2xwuGmww',
+        client_token: '/XvkYoXDLJVJqSZ8gLTLf9ACpSSPXxMoipgpBXF5Y1qZLSjzvYY8CM999jCU4E+/FPbKWxIq+RpC/i+RCSso3tsXxcCqWOi7U2ri1UgYruOwz2CIR91vUe0='
+    },
+    uber: {
+        client_id: 'doWL3QHitTKpFMY-n9HoHFfoPLOYjxwW',
+        client_secret: 'syzKGUg4isEhqKO3ISxODnVhDKUGJa7wuWnEFHHG',
+        server_token: 'V68RJovI6UHerPL7H8LIPw3Wq3wkWE1UFQGm7WBi'
+    },
+    twitter: {
+        consumer_key: 'ys889oiisvbmbeSiFYytd4vsc',
+        consumer_secret: 'Oc5lu89Ur1dn4G1vsUnUchCvfNk76Oe79eQfzGnpDCKVwHAguA',
+        access_token: '984987929130029056-pig9J59DsNimBqP8V4Rmg7b9braqKhr',
+        access_token_secret: '1HEGIz4zoEJXd2ytFTRJoHVuXNJWiUaK7xpU2kVgvGR1v',
+        TAG: 'traveltimetogo'
+    },
+    google: {
+        key: 'AIzaSyDZ3biqwWtqAmaacrdvf77OpL0gqF0gqEk'
+    }
+}
