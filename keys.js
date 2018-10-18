796ac74eef9fe2cbd68f4d8835b22242e765f2bf
Dennis George <djg365@drexel.edu>
2018-10-17 22:02:11 -0400

    update keys

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 module.exports = {
     lyft: {
         client_id: '-fGhLUA51FgR',
-        client_secret: 'H9F56Y1rO-8YVBY7ujYx7P0H2xwuGmww',
-        client_token: '/XvkYoXDLJVJqSZ8gLTLf9ACpSSPXxMoipgpBXF5Y1qZLSjzvYY8CM999jCU4E+/FPbKWxIq+RpC/i+RCSso3tsXxcCqWOi7U2ri1UgYruOwz2CIR91vUe0='
+        client_secret: 'DSyahSFTQ3creI_a5EFwcgpFsGZmplgd',
+        client_token: 'hWthEh21IkjoRaCukhqtUiI1wjwkTlvDncfFOxkEQf5LGDSPFAnDbaPvQAnrVO828lfnR8+RpEaWiSeBXzWNQx29cp72TQTrkArRTwweqSzow08R9VgkPOI='
     },
     uber: {
         client_id: 'doWL3QHitTKpFMY-n9HoHFfoPLOYjxwW',
-        client_secret: 'syzKGUg4isEhqKO3ISxODnVhDKUGJa7wuWnEFHHG',
-        server_token: 'V68RJovI6UHerPL7H8LIPw3Wq3wkWE1UFQGm7WBi'
+        client_secret: 'qyx0mrxUXhWSE6HA0jz43O6zuc_JQBsc1TPFXD73',
+        server_token: 'kOfGL0b6dDpLh5jOAS_WXRF755Fobl2VEWMjiuZl'
     },
     twitter: {
         consumer_key: 'fEKynb8VdaKBryIVAGGv4UeOy', 
